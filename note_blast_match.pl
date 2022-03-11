4db754e25243a19372f1d6c75d87020f99816cc5
earnan <hi@arcsona.cn>
2022-03-11 16:07:33 +0800

    1

--- note_blast_match.pl
+++ note_blast_match.pl
@@ -49,12 +49,14 @@ while (<IN>) {
 	next if (/^$/); #如果没有换行符就跳出循环
 	if (/>/) {  #开头有>的话
         #print "$_\n";
-		$id=$_;
-        print "$id\n";
+		$id=$_;#$_表示默认输出
+        #print "$id\n";
 
 	}else{
-		$cds_subject{$id}.=$_;
-		$cds_len{$id} = length $cds_subject{$id};
+		$cds_subject{$id}.=$_; #id对应   序列
+		#print "$_\n";
+		$cds_len{$id} = length $cds_subject{$id};#id--lenth
+		#print "$cds_len{$id}\n"
 	}
 }
 
@@ -67,54 +69,61 @@ while (<IN>) {
 		$id=$_;
 	}else{
 		$cds_query{$id}.=$_;
+		
 	}
 }
 
 ###############
 my %cds_homo;
 open (IN,"$outdir/blastn.tophit.result.xls") or die $!;
-<IN>;
+<IN>;#跳过第一行
 while (<IN>) {
 	chomp;
 	my ($qid,$q_length,$q_aln_start,$q_aln_end,$s_length,$s_aln_start,$s_aln_end,$sid)=(split/\t/,$_)[0,1,2,3,5,6,7,15];
 
+=pod
 	if($q_aln_start > $q_aln_end or $s_aln_start > $s_aln_end or ($q_aln_end - $q_aln_start + 1) / $q_length < 0.6 or ($s_aln_end - $s_aln_start + 1)/$s_length < 0.6){
-		next;
+		next;#跳过后咋办??????????????????
 	}
-
-	my ($ncbi,$q_pos)=(split/\s+/,$qid);
-	my ($q_start,$q_end)=$q_pos=~/(\d+)/g;
+=cut
+	my ($ncbi,$q_pos)=(split/\s+/,$qid);#被任意数量的空格分开 +表示任意数量
+	
+	my ($q_start,$q_end)=$q_pos=~/(\d+)/g;#全局匹配数字  ???????
+	#print $q_start,$q_end;
+	#print "\n";
 	my $s_pos=(split/\s+/,$sid)[1];
 #	my ($s_start,$s_end)=$s_pos=~/(\d+)/g;
 	my ($s_start,$s_end)=$s_pos=~/(\d+)/g;
 #	my $dis=abs($q_start-$s_start);
 	my $dis=length $cds_query{">".$qid};
+	#print ">".$qid  与 上面print $id  相同
 
-	push @{$cds_homo{">".$sid}},[$ncbi,$dis,">".$qid];
+	push @{$cds_homo{">".$sid}},[$ncbi,$dis,">".$qid];#数组  suject完整信息的哈希, query的名字 长度 query完整带>带基因名的名字
 }
 
 ###############
 my @output;
-foreach my $k (keys %cds_homo) {
+foreach my $k (keys %cds_homo) {  #$k为subject完整名字带基因名
 	my ($NCBI_id,$pos,$gene,$product)=split/\s+/,$k;
-	my ($start,$end)=$pos=~/(\d+)/g;
+	my ($start,$end)=$pos=~/(\d+)/g;#同上疑惑
 	($gene)=$gene=~/gene=(.*?)]/;
 
-	my @fasta=("$k\n".$cds_subject{$k});
-	my $len = $cds_len{$k};
-	print "$k len: $len\n";
+	my @fasta=("$k\n".$cds_subject{$k});#把MmG1的名字序列放一串字符串里
+	my $len = $cds_len{$k};#G1长度
+	print "$k len: $len\n";#物种基因情况
 	my %filter;
 	my @homo_group=@{$cds_homo{$k}};
+
 
 	@homo_group=sort { 
 		$$a[0] cmp $$b[0] 
 		or 
-		$$b[1]<=>$$a[1]} @homo_group;
+		$$b[1]<=>$$a[1]} @homo_group;#排序规则,对Mmg1的每个subject完整信息排序
 
 	my @filter_homo=grep {
 		if (!exists $filter{$_->[0]}) {
 			$filter{$_->[0]}=1;
-			print "$_->[0] $_->[1]\n";
+			print "$_->[0] $_->[1]\n";#其他物种的基因长度
 		}
 	}@homo_group;
 
