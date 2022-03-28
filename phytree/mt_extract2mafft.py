d8b7e07d6618b3203453ec9e2b96064d33e7e10e
earnan <hi@arcsona.cn>
2022-03-28 17:01:36 +0800

    新建

---
+++ phytree/mt_extract2mafft.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding : utf-8 -*-
+##########################################################
+#
+#       Filename:   mt_extract2mafft.py
+#         Author:   yujie
+#    Description:   mt_extract2mafft.py
+#        Version:   1.0
+#           Time:   2022/03/28 17:00:58
+#  Last Modified:   2022/03/28 17:00:58
+#        Contact:   hi@arcsona.cn
+#        License:   Copyright (C) 2022
+#
+##########################################################
