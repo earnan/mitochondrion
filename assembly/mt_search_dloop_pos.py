0509b7e5aaf25f018fa23d5bf4d721c786f1f7c0
earnan <hi@arcsona.cn>
2022-08-12 14:22:32 +0800

    1

--- assembly/mt_search_dloop_pos.py
+++ assembly/mt_search_dloop_pos.py
@@ -14,7 +14,7 @@
 ##########################################################
 from Bio import SeqIO
 from Bio.Seq import Seq
-from icecream import ic
+#from icecream import ic
 import argparse
 import linecache
 import os
@@ -26,19 +26,13 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(
     add_help=False, usage='\
 \npython3   mt_search_dloop_pos.py\n\
-step1\n\
-step2\n\
 V1.0')
 optional = parser.add_argument_group('可选项')
 required = parser.add_argument_group('必选项')
 optional.add_argument(
-    '-i', '--infile', metavar='[infile]', help='infile', type=str, default='F:/4228/nd/K125.coverage.txt', required=False)
+    '-i', '--infile', metavar='[infile]', help='K125.coverage.txt', type=str, default='F:/4228/nd/K125.coverage.txt', required=False)
 optional.add_argument(
     '-o', '--outfile', metavar='[outfile]', help='outfile', type=str, default='F:/', required=False)
-optional.add_argument('-c1', '--flag1', help='run step 1?默认是,不运行则-c1',
-                      action='store_false', required=False)
-optional.add_argument('-c2', '--flag2', help='run step 2?默认否,运行则-c2 ',
-                      action='store_true', required=False)
 optional.add_argument('-h', '--help', action='help', help='[帮助信息]')
 args = parser.parse_args()
 
